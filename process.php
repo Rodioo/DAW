ecb331ac45401794964d47dbcb6155d1bb2f0c2d
Antonio Falcescu <79379869+Rodioo@users.noreply.github.com>
2021-12-04 22:47:25 +0200

    Add files via upload

--- process.php
+++ process.php
@@ -1,6 +1,14 @@
 <?php
 
-    require_once('connection.php');
+    //require_once('connection.php');
+    $host = "eu-cdbr-west-01.cleardb.com";
+    $user = "b3d50e11cd16c4";
+    $pass = "a7b0f45d";
+    $db = "heroku_ab134baabfb7904";
+
+    $con = mysqli_connect($host, $user, $pass);
+    mysqli_select_db($con, $db);
+
 
     if(isset($_POST['btn-save']))
     {
