10cd48a8276308b919d4c0e511c96cd149b65115
Antonio Falcescu <79379869+Rodioo@users.noreply.github.com>
2021-12-04 22:39:16 +0200

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_url = parse_url(getenv("mysql://b3d50e11cd16c4:a7b0f45d@eu-cdbr-west-01.cleardb.com/heroku_ab134baabfb7904?reconnect=true"));
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
