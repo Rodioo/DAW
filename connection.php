0d4cdac6b44671c9b88f645d5e60d8682fabb4f0
Antonio Falcescu <79379869+Rodioo@users.noreply.github.com>
2021-12-04 22:33:07 +0200

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,9 +1,14 @@
 <?php
-
-    $con = mysqli_connect('localhost', 'root','ceva28439', 'proiect');
-    if(!$con)
-    {
-        echo 'Eroare la conectarea la baza de date.';
-    }
-
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+if(!$conn)
+    echo "Eroare la conectarea la baza de date.";
 ?>
--- login.php
+++ login.php
@@ -2,10 +2,10 @@
 
     session_start();
 
-    $host = "localhost";
-    $user = "root";
-    $pass = "ceva28439";
-    $db = "proiect";
+    $host = "eu-cdbr-west-01.cleardb.com";
+    $user = "b3d50e11cd16c4";
+    $pass = "a7b0f45d";
+    $db = "heroku_ab134baabfb7904";
 
     $con = mysqli_connect($host, $user, $pass);
     mysqli_select_db($con, $db);
